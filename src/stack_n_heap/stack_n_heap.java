93f6bc6921884b8b0cdbcacbf5c19b36984986ae
Bayu Kembara <bayu@teavaro.com>
2023-09-01 10:55:41 +0700

    Stack_N_Heap

---
+++ src/stack_n_heap/stack_n_heap.java
@@ -0,0 +1,8 @@
+package stack_n_heap;
+
+public class stack_n_heap {
+    public static void main(String[] args) {
+//        Stack is local variables, primitive data type
+//        and if you have a lot of variables and data using heap
+    }
+}
