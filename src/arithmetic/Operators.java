910f8dd4aa356c6e15b9af64b4947a9abca37cbe
Bayu Kembara <bayu@teavaro.com>
2023-09-01 16:16:57 +0700

    Operators

---
+++ src/arithmetic/Operators.java
@@ -0,0 +1,20 @@
+package arithmetic;
+
+public class Operators {
+    public static void main(String[] args) {
+        /*
+        *  + is for additive operators
+        *  - is for subtract operators
+        *  * is for multiply operators
+        *  / is for divide operators
+        *  % is for modulo operators
+        *
+        * */
+
+        double n1 = 8.4,n2=4.2,result = 0.0;
+
+
+
+    }
+}
+
