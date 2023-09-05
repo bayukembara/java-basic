9f1479e8528ef4d4cc3bb73f28c0a4bc2782e952
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-09-05 08:48:55 +0700

    Arithmetic

--- src/arithmetic/Expression_Statement_Blocks.java
+++ src/arithmetic/Expression_Statement_Blocks.java
@@ -1,4 +1,26 @@
 package arithmetic;
 
 public class Expression_Statement_Blocks {
+
+	public static void main(String[] args) {
+//* Expressions
+		int maxSpeed = 280;
+		int result = 5 + 8;
+		double x = 10.6,y = 6.4, result1;
+		result1 = (x+y)*2.5;
+		if(number1==number2)
+			System.out.println("Equal");
+
+//* Statements
+		int result = 7 * 6;
+		result ++;
+		System.out.println("Hello World");
+
+//* Blocks
+		boolean control =  true;
+		if(control){
+//! Begin block
+			System.out.println("Hello World");
+		}//! End block
+	}
 }
