88e56de17f921ef737a0a6013b0cc6227dddb3b8
Bayu Kembara <bayu@teavaro.com>
2023-09-07 13:49:55 +0700

    if and nested if

---
+++ src/decisionmaking/ifelsestatement.java
@@ -0,0 +1,18 @@
+package decisionmaking;
+
+import java.util.Scanner;
+
+public class ifelsestatement {
+    public static void main(String[] args) {
+        Scanner inputNumber  = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int number = inputNumber.nextInt();
+
+        if(number % 2 == 0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
+        inputNumber.close();
+    }
+}
